f53469eaddbe387950800c4647ddd62c80675a99
rakeshnagar@hotmail.com <rakeshnagar@hotmail.com>
2016-04-17 16:52:10 -0400

    initial commit

---
+++ flyway-mysql/src/main/resources/db/migration/V1.2__Create_address_table.sql
@@ -0,0 +1,7 @@
+create table ADDRESS (
+    LINE1 varchar(200) not null,
+    LINE2 varchar(200) null,
+    STATE varchar(50) not null,
+    ZIPCODE varchar(50) not null,
+    TYPE varchar(100) not null
+);
